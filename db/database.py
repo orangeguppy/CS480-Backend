34b4656d33845f622ad5b4e2b6e9a8b83387bc1c
orangeguppy <vuwet467@gmail.com>
2024-11-08 22:52:19 +0800

    rpi3

--- db/database.py
+++ db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:example_password@db:3306/cs480"
+DATABASE_URL = "mysql+pymysql://root:password@db:3306/cs480"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
